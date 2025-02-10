ffce6654a6256eb0d14d44db62e4fa7afebbf21b
Denis Korolevkii <diniskorolevkij@gmail.com>
2025-02-10 20:53:41 +0600

    Создал пустой проект

---
+++ ToDoTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoTest
+//
+//  Created by Денис Королевский on 10/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
